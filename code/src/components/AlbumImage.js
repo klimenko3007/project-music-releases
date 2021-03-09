ecc9fe0815b35b69e83e2c1f3337146910618df6
Ekaterina Klimenko <klimenko3007@gmail.com>
2021-03-09 23:59:49 +0100

    Added play buttons and styled them

--- code/src/components/AlbumImage.js
+++ code/src/components/AlbumImage.js
@@ -3,7 +3,15 @@ import React from 'react'
 const AlbumImage = (album) => {
     return (
         <div className="image-container">
-            <img className="image" src={album.picture} alt="Album"/>
+            <img className="image" src={album.picture} alt="Album"
+            />
+            <div className="buttons-container">
+                <div className="wrapper">
+                    <img className="heart" src="./icons/heart.svg" alt="Heart"/>
+                    <img className="play-button" src="./icons/play.svg" alt="Play button"/>
+                    <img className="dots" src="./icons/dots.svg" alt="dots" />
+                </div>
+            </div>
         </div>
     )
 
