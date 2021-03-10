fc1533d9a858a07ff37905257aa9158004170705
Ekaterina Klimenko <klimenko3007@gmail.com>
2021-03-10 20:35:36 +0100

    Blue level almost complete

--- code/src/AlbumImage.js
+++ code/src/AlbumImage.js
@@ -7,11 +7,11 @@ const AlbumImage = (prop) => {
                   <img className="album-image"src={prop.album.images[1].url} alt="Album"/>
                   <div className="icons-container">
                     <div className="hover-container">
-                        <img className="icon" src="./icons/heart.svg" alt="heart icon"/>
+                        <img className="heart" src="./icons/heart.svg" alt="heart icon"/>
                         <a href={prop.album.external_urls.spotify} title="link to spotify" target="_blank">
-                            <img className="icon" src="./icons/play.svg" alt="play button" />
+                            <img className="play" src="./icons/play.svg" alt="play button" />
                         </a>
-                        <img className="icon" src="./icons/dots.svg" alt="dots icon" />
+                        <img className="dots" src="./icons/dots.svg" alt="dots icon" />
                     </div>
                   </div>
               </div>
