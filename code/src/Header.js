cc1ddcca1b6f637633cbadaed4c5b60fe20afec6
Ekaterina Klimenko <klimenko3007@gmail.com>
2021-03-10 19:15:39 +0100

    Added hamburger, logo, styled grid

--- code/src/Header.js
+++ code/src/Header.js
@@ -3,8 +3,8 @@ import React from 'react';
 const Header = () => {
     return (
         <header className="header">
-            <img className="logo" src="" alt="logo" />
-            <img className="hamburger" src="./icons/heart.svg" alt="hamburger"/>
+            <img className="logo" src="./icons/logo-music-project_font_white_V2.png" alt="logo" />
+            <img className="hamburger" src="./icons/hamburger.png" alt="hamburger"/>
             <nav className="nav">
                 <a className="nav-item" href="#">placeholder</a>
                 <a className="nav-item" href="#">placeholder</a>
