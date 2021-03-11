1f46d45b3fc70844e4798a08f2731271b80a888b
Ekaterina Klimenko <klimenko3007@gmail.com>
2021-03-11 19:20:02 +0100

    Red level complete

--- code/src/Album.js
+++ code/src/Album.js
@@ -9,8 +9,8 @@ export const Album = prop => {
             <AlbumImage album={prop.album_data}/>
             <a href={prop.album_data.external_urls.spotify} className="album-name" target="_blank" rel="noopener noreferrer">{prop.album_data.name} </a>
             <div className="artist-container">
-            {prop.album_data.artists.map((item) => {
-                 return <Artist key={item.id} artist={item}/>
+            {prop.album_data.artists.map((item, index, array) => {
+                 return <Artist key={item.id} artist={item} artist_index={index} artist_array={array}/>
             })}
             </div>  
         </div>        
--- code/src/Artist.js
+++ code/src/Artist.js
@@ -1,11 +1,21 @@
 import React from 'react';
 
+const generateDelimiter = (index, length) => {
+  if (index === length - 2) {
+    return " & ";
+  } else if (index < length - 2) {
+    return ", ";
+  } else{
+  return;
+  }
+};
 
 const Artist = (prop) => {
     console.log("Artist", prop)
     return (
         <a className="artist-name" title="link to the artist" target="_blank" rel="noopener noreferrer" href={prop.artist.external_urls.spotify}>
             {prop.artist.name}
+            {generateDelimiter(prop.artist_index, prop.artist_array.length)}
         </a> 
     )
 }
