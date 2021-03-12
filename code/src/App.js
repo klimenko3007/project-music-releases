91e43fe85a4d4262e15166a1902bca399d3a2c9d
Ekaterina Klimenko <klimenko3007@gmail.com>
2021-03-12 15:35:05 +0100

    Created Section component, cleaned up App

--- code/src/App.js
+++ code/src/App.js
@@ -1,22 +1,23 @@
 import React from 'react'
 import data from './data.json'
 import stretchGoal from './stretch-goal.json' 
-import {Album} from './Components/Album.js'
 import Footer from './Components/Footer.js'
 import Header from './Components/Header.js'
 import Playlist from './Components/Playlist.js'
+import Section from './Components/Section.js'
 
-console.log(stretchGoal)
+// Array of playlists for stretchgoals
 const playlistArray =stretchGoal.playlists.items
+// Array of items from data
 const albumArray = data.albums.items
+// Array of singles 
 const singlesArray = albumArray.filter(single => {
   return single.album_type.includes("single")
 } )
+// Array of albums
 const onlyAlbumArray = albumArray.filter(album =>{
   return album.album_type.includes("album")
-}
-  )
-  //console.log(onlyAlbumArray);
+})
 
 export const App = () => {
   return (
@@ -24,25 +25,9 @@ export const App = () => {
       <Header />
       <div className="main">
         <Playlist array={playlistArray}/>
-        {/* <Section array={singlesArray} title="Singles"/>
-        <Section array={onlyAlbumArray} title="Albums"/> */}
         <div className="section-container">
-          <section className="section">
-            <h1 className="section-heading">Singles</h1>
-            <div className="album-container">
-              {singlesArray.map((album) =>{
-                return <Album key={album.id} album_data={album}/>
-            })}
-            </div>
-          </section>
-          <section className="section">
-            <h1 className="section-heading">Albums</h1>
-            <div className="album-container">
-              {onlyAlbumArray.map((album) =>{
-                return <Album key={album.id} album_data={album}/>
-            })}
-            </div>
-          </section>
+          <Section array={singlesArray} title="Singles" />
+          <Section array={onlyAlbumArray} title="Albums" />
         </div>
       </div>
       <Footer />
---
+++ code/src/Components/Section.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Album } from './Album.js';
+const Section = (props) => {
+    return (
+        <section className="section">
+            <h1 className="section-heading">{props.title}</h1>
+            <div className="album-container">
+            {props.array.map((album) =>{
+                return <Album key={album.id} album_data={album}/>
+            })}
+            </div>
+        </section>
+    )
+}
+export default Section;
